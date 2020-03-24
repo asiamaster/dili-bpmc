dbd919da23c7897502c87a3caf744b210e230e98
wangmi <87730066>
2020-03-24 17:27:27 +0800

    为了不影响上线，去掉服务编排功能的代码

--- bpmc/src/main/java/com/dili/bpmc/service/SendMsgService.java
+++ bpmc/src/main/java/com/dili/bpmc/service/SendMsgService.java
@@ -1,9 +1,6 @@
 package com.dili.bpmc.service;
-import java.util.Date;
 
 import com.dili.bpmc.dao.ActFormMapper;
-import com.dili.bpmc.sdk.domain.ActForm;
-import com.dili.ss.dto.DTOUtils;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,21 +13,21 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class SendMsgService implements JavaDelegate {
 
-    @Autowired
-    ActFormMapper actFormMapper;
+//    @Autowired
+//    ActFormMapper actFormMapper;
     @Override
     @Transactional
     public void execute(DelegateExecution execution) throws Exception {
-        ActForm actForm = DTOUtils.newInstance(ActForm.class);
-        actForm.setId(999L);
-        actForm.setFormKey("test");
-        actForm.setDefKey("test");
-        actForm.setActionUrl("");
-        actForm.setTaskUrl("");
-        actForm.setRedirectUrl("");
-        actForm.setCreated(new Date());
-        actForm.setModified(new Date());
-        actFormMapper.insert(actForm);
+//        ActForm actForm = DTOUtils.newInstance(ActForm.class);
+//        actForm.setId(999L);
+//        actForm.setFormKey("test");
+//        actForm.setDefKey("test");
+//        actForm.setActionUrl("");
+//        actForm.setTaskUrl("");
+//        actForm.setRedirectUrl("");
+//        actForm.setCreated(new Date());
+//        actForm.setModified(new Date());
+//        actFormMapper.insert(actForm);
         System.out.println("实例id["+execution.getProcessInstanceId() +"],活动id["+execution.getCurrentActivityId()+"]发消息");
     }
 }
--- bpmc/src/main/java/com/dili/bpmc/servicetask/RibbonService.java
+++ bpmc/src/main/java/com/dili/bpmc/servicetask/RibbonService.java
@@ -1,94 +1,94 @@
-package com.dili.bpmc.servicetask;
-
-import com.dili.ss.domain.BaseOutput;
-import org.activiti.engine.delegate.DelegateExecution;
-import org.activiti.engine.delegate.Expression;
-import org.activiti.engine.delegate.JavaDelegate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import javax.annotation.PostConstruct;
-import java.util.Map;
-
-/**
- * Ribbon远程服务任务
- * @Author: WangMi
- * @Date: 2020/3/24 10:53
- * @Description:
- */
-@Service
-@ConditionalOnClass(RestTemplate.class)
-public class RibbonService implements JavaDelegate {
-
-    public final static Logger log = LoggerFactory.getLogger(RibbonService.class);
-    @PostConstruct
-    public void init(){
-        System.out.println("RibbonService initd");
-    }
-    //ribbon URL
-    private Expression url;
-    //重试次数
-    private Expression retry;
-    //重试间隔(秒)
-    private Expression interval;
-    @Autowired
-    RestTemplate restTemplate;
-    @Override
-    public void execute(DelegateExecution execution) {
-        Object urlObj = url.getValue(execution);
-        if(urlObj == null){
-            return;
-        }
-        //默认重试三次
-        int retryInt = 3;
-        //默认三秒重试
-        long intervalLong = 3000;
-        //重试次数
-        try {
-            if(retry != null) {
-                retryInt = Integer.parseInt(retry.getValue(execution).toString().trim());
-            }
-            if(interval != null) {
-                intervalLong = Long.parseLong(interval.getValue(execution).toString().trim() + "000");
-            }
-        }catch (Exception e){
-            //数字转换异常时，使用默认参数
-        }
-        while(retryInt > 0) {
-            try {
-                ResponseEntity<BaseOutput> responseEntity = restTemplate.postForEntity(urlObj.toString(), execution.getVariables(), BaseOutput.class);
-                if(HttpStatus.OK.equals(responseEntity.getStatusCode())){
-                    BaseOutput<Map<String, Object>> baseOutput = responseEntity.getBody();
-                    if (baseOutput.isSuccess()) {
-                        execution.setVariables(baseOutput.getData());
-                        break;
-                    } else {
-                        retryInt--;
-                        log.error(String.format("ribbon调用[" + urlObj.toString() + "]内部异常, code:[%s], message:[%s]", baseOutput.getCode(), baseOutput.getMessage()));
-                        Thread.sleep(intervalLong);
-                    }
-                }else{
-                    retryInt--;
-                    log.error(String.format("ribbon调用[" + urlObj.toString() + "]发生失败,%s秒后重试,StatusCodeValue:[%s]", intervalLong/1000L, responseEntity.getStatusCodeValue()));
-                    Thread.sleep(intervalLong);
-                }
-            } catch (InterruptedException e ) {
-                log.error(String.format("远程调用[" + url + "]发生异常,message:[%s],%s秒后重试", e.getMessage(), intervalLong/1000L));
-                retryInt--;
-                try {
-                    Thread.sleep(intervalLong);
-                } catch (InterruptedException ex) {
-                    //dont care
-                }
-            }
-        }
-    }
-
-}
+//package com.dili.bpmc.servicetask;
+//
+//import com.dili.ss.domain.BaseOutput;
+//import org.activiti.engine.delegate.DelegateExecution;
+//import org.activiti.engine.delegate.Expression;
+//import org.activiti.engine.delegate.JavaDelegate;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.client.RestTemplate;
+//
+//import javax.annotation.PostConstruct;
+//import java.util.Map;
+//
+///**
+// * Ribbon远程服务任务
+// * @Author: WangMi
+// * @Date: 2020/3/24 10:53
+// * @Description:
+// */
+//@Service
+//@ConditionalOnClass(RestTemplate.class)
+//public class RibbonService implements JavaDelegate {
+//
+//    public final static Logger log = LoggerFactory.getLogger(RibbonService.class);
+//    @PostConstruct
+//    public void init(){
+//        System.out.println("RibbonService initd");
+//    }
+//    //ribbon URL
+//    private Expression url;
+//    //重试次数
+//    private Expression retry;
+//    //重试间隔(秒)
+//    private Expression interval;
+//    @Autowired
+//    RestTemplate restTemplate;
+//    @Override
+//    public void execute(DelegateExecution execution) {
+//        Object urlObj = url.getValue(execution);
+//        if(urlObj == null){
+//            return;
+//        }
+//        //默认重试三次
+//        int retryInt = 3;
+//        //默认三秒重试
+//        long intervalLong = 3000;
+//        //重试次数
+//        try {
+//            if(retry != null) {
+//                retryInt = Integer.parseInt(retry.getValue(execution).toString().trim());
+//            }
+//            if(interval != null) {
+//                intervalLong = Long.parseLong(interval.getValue(execution).toString().trim() + "000");
+//            }
+//        }catch (Exception e){
+//            //数字转换异常时，使用默认参数
+//        }
+//        while(retryInt > 0) {
+//            try {
+//                ResponseEntity<BaseOutput> responseEntity = restTemplate.postForEntity(urlObj.toString(), execution.getVariables(), BaseOutput.class);
+//                if(HttpStatus.OK.equals(responseEntity.getStatusCode())){
+//                    BaseOutput<Map<String, Object>> baseOutput = responseEntity.getBody();
+//                    if (baseOutput.isSuccess()) {
+//                        execution.setVariables(baseOutput.getData());
+//                        break;
+//                    } else {
+//                        retryInt--;
+//                        log.error(String.format("ribbon调用[" + urlObj.toString() + "]内部异常, code:[%s], message:[%s]", baseOutput.getCode(), baseOutput.getMessage()));
+//                        Thread.sleep(intervalLong);
+//                    }
+//                }else{
+//                    retryInt--;
+//                    log.error(String.format("ribbon调用[" + urlObj.toString() + "]发生失败,%s秒后重试,StatusCodeValue:[%s]", intervalLong/1000L, responseEntity.getStatusCodeValue()));
+//                    Thread.sleep(intervalLong);
+//                }
+//            } catch (InterruptedException e ) {
+//                log.error(String.format("远程调用[" + url + "]发生异常,message:[%s],%s秒后重试", e.getMessage(), intervalLong/1000L));
+//                retryInt--;
+//                try {
+//                    Thread.sleep(intervalLong);
+//                } catch (InterruptedException ex) {
+//                    //dont care
+//                }
+//            }
+//        }
+//    }
+//
+//}
